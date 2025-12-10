94976d3d7723de4e8878b51f55e0455a5938fb2f
Justin Frevert <justinfrevert@gmail.com>
2025-12-10 10:44:20 -0800

    log target

--- ledger/src/versions/common/api/ledger.rs
+++ ledger/src/versions/common/api/ledger.rs
@@ -189,7 +189,7 @@ impl<D: DB> Ledger<D> {
 	) -> Result<Sp<Self, D>, LedgerApiError> {
 		let block_fullness = sp.block_fullness.clone().into();
 
-		log::debug!("post_block_update - tblock: {:?}, block fullness: {:?} block context: {:?}", Timestamp::from_secs(block_context.tblock), block_fullness, block_context);
+		log::debug!(target: LOG_TARGET, "post_block_update - tblock: {:?}, block fullness: {:?} block context: {:?}", Timestamp::from_secs(block_context.tblock), block_fullness, block_context);
 		let next_state = sp
 			.state
 			.post_block_update(Timestamp::from_secs(block_context.tblock), block_fullness)
