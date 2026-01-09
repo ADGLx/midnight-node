f9708c491008df507796b1ce9e08a82049821cc0
Justin Frevert <justinfrevert@gmail.com>
2026-01-09 10:26:14 -0800

    follow up

--- node/src/main_chain_follower.rs
+++ node/src/main_chain_follower.rs
@@ -296,7 +296,9 @@ async fn get_connection(
 	pool_cfg: DbPoolCfg,
 	allow_non_ssl: bool,
 ) -> Result<sqlx::PgPool, Box<dyn Error + Send + Sync + 'static>> {
-	let connect_options = sqlx::postgres::PgConnectOptions::from_str(connection_string)?;
+	let connect_options = sqlx::postgres::PgConnectOptions::from_str(connection_string)?
+      .ssl_mode(sqlx::postgres::PgSslMode::Disable);
+
 	let pool = sqlx::postgres::PgPoolOptions::new()
 		.max_connections(pool_cfg.max_connections)
 		.acquire_timeout(pool_cfg.acquire_timeout)
